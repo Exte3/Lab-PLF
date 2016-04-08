b705811a2d3ceb6eed66f13aa30a4763b69f9814
Gabriel Fuentes <nunkmeconecto@gmail.com>
2016-04-07 21:48:14 -0300

    Empezando
    Ahora reconoce acentos

---
+++ code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+main(int argc, char *argv[]){
+	if(argc==1){
+		printf("Error: Faltan parámetros en la línea de comandos. %s\n");
+	}
+	if(argc==2){
+
+	}
+	if(argc==3){
+
+	}
+	if(argc==4){
+
+	}
+	return 0;
+}
---
+++ fechas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main(int argc, char *argv[]){
+	if(argc==1){
+		printf("Error: Faltan par%cmetros en la l%cnea de comandos. %s\n", 160, 161);
+	}
+	if(argc==2){
+	}
+	if(argc==3){
+	}
+	if(argc==4){
+	}
+	else{
+	}
+	return 0;
+}
