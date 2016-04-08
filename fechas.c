d44827ca717eb0c45f951f01acbc03a309a9892a
Gabriel Fuentes <nunkmeconecto@gmail.com>
2016-04-07 22:50:47 -0300

    Mejora
    Actualmente ya puede imprimir con caracter con acento sin el menor
    problema

--- fechas.c
+++ fechas.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-
+#include <locale.h>
 main(int argc, char *argv[]){
+	#Con este parametro se pueden usar acentos y la biblioteca locate
+	setlocale(LC_ALL, "spanish");
+
 	if(argc==1){
-		printf("Error: Faltan par%cmetros en la l%cnea de comandos. %s\n", 160, 161);
+		printf("Error: Faltan parámetros en la línea de comandos. \n");
 	}
 	if(argc==2){
 	}
 	if(argc==3){
+	//correcto, ambos archivos más el nombre del programa
 	}
 	if(argc==4){
+		printf("Error: Demasiados parámetros en la línea de comandos. \n");
 	}
 	else{
 	}
+	
 	return 0;
 }
