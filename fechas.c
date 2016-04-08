80a3295c9a57d85861b22b4441c83d2a2618c9af
Gabriel Fuentes <nunkmeconecto@gmail.com>
2016-04-07 23:00:32 -0300

    Mensajes
    Al parecer todos los mensajes por parametros listos

--- fechas.c
+++ fechas.c
@@ -6,14 +6,20 @@ main(int argc, char *argv[]){
 
 	if(argc==1){
 		printf("Error: Faltan parámetros en la línea de comandos. \n");
+		printf("Uso: fechas.exe archivo_entrada archivo_salida \n");
+		//en este caso no se puede crear salida
+		//muy importante
 	}
 	if(argc==2){
+		printf("Error: Falta parámetro en la línea de comandos. \n");
+		printf("Uso: fechas.exe archivo_entrada archivo_salida \n");
 	}
 	if(argc==3){
 	//correcto, ambos archivos más el nombre del programa
 	}
-	if(argc==4){
+	if(argc>=4){
 		printf("Error: Demasiados parámetros en la línea de comandos. \n");
+		printf("Uso: fechas.exe archivo_entrada archivo_salida \n");
 	}
 	else{
 	}
