5598028479bd562c7a5178ee2bb22b2d096fa8d6
Gabriel Fuentes <nunkmeconecto@gmail.com>
2016-05-03 00:15:19 -0300

    Aveces solo queda el estilo mono
    No encontre una forma de que fueran elegantes las palabras...

--- fechas.c
+++ fechas.c
@@ -1,10 +1,48 @@
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
+#include <stdlib.h>
+typedef struct dia{
+	char *num;
+	struct dia *sgt;
+}dia;
+dia *asigmem(){
+	dia *cabeza;
+	cabeza=malloc(sizeof(dia));
+	dia *sgti;
+	sgti=malloc(sizeof(dia));
+	cabeza->sgt=sgti;
+	int i = 0;
+	while(i == 12){
+		if(sgti==NULL){
+			sgti = malloc(sizeof(dia));
+		}
+		sgti=sgti->sgt;
+		i++;
+	}
+	return cabeza;
+}
+dia *creardias(dia *cabeza){
+	cabeza->num = "1";
+	cabeza->sgt->num = "2";
+	cabeza->sgt->sgt->num = "3";
+	cabeza->sgt->sgt->sgt->num = "4";
+	cabeza->sgt->sgt->sgt->sgt->num = "5";
+	cabeza->sgt->sgt->sgt->sgt->sgt->num = "6";
+	cabeza->sgt->sgt->sgt->sgt->sgt->sgt->num = "7";
+	cabeza->sgt->sgt->sgt->sgt->sgt->sgt->sgt->num = "8";
+	cabeza->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->num = "9";
+	cabeza->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->num = "10";
+	cabeza->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->num = "11";
+	cabeza->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->sgt->num = "12";
+	
+	return cabeza;
+}
 main(int argc, char *argv[]){
 	//Con este parametro se pueden usar acentos y la biblioteca locate
 	setlocale(LC_ALL, "spanish");
-
+	asigmem();
+	//creardias();
 	if(argc==1){
 		printf("Error: Faltan parámetros en la línea de comandos. \n");
 		printf("Uso: fechas.exe archivo_entrada archivo_salida \n");
@@ -34,12 +72,8 @@ main(int argc, char *argv[]){
 				token = strtok(linea,"-/");
 				//printf("%s", token);
 				while(token!=NULL){
-					printf("%s\n", token);
+					printf("%s", token);
 					token = strtok(NULL,"-/");
-					c = strcmp(linea, token);
-					if(c==0){
-						printf("todo se derrumbo /n");
-					}
 				}
 				
 			}
